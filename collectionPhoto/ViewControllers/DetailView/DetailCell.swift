a13a276eadcf39193496430618e6081424c24dc1
Roman Nazarov <ste7ww@gmail.com>
2022-07-30 13:20:43 +0500

    addDetailCell

--- collectionPhoto/ViewControllers/DetailView/DetailCell.swift
+++ collectionPhoto/ViewControllers/DetailView/DetailCell.swift
@@ -5,4 +5,41 @@
 //  Created by Роман Назаров on 30.07.2022.
 //
 
-import Foundation
+import UIKit
+
+class DetailCell: UICollectionViewListCell {
+    static let reuseIdentifer = "detail-cell"
+    
+    let imageView = UIImageView()
+    let contentContainer = UIView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        contentContainer.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(contentContainer)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        contentContainer.addSubview(imageView)
+        imageView.backgroundColor = .gray
+        NSLayoutConstraint.activate([
+            contentContainer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            contentContainer.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            contentContainer.topAnchor.constraint(equalTo: contentView.topAnchor),
+            contentContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            imageView.leadingAnchor.constraint(equalTo: contentContainer.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentContainer.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentContainer.bottomAnchor),
+            imageView.topAnchor.constraint(equalTo: contentContainer.topAnchor)
+        ])
+    }
+}
