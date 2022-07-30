fcb04066c3666d8087ca329a5937a5bca3e07a27
Roman Nazarov <ste7ww@gmail.com>
2022-07-30 13:19:22 +0500

    addSearchViewController

--- collectionPhoto/ViewControllers/TabBarViewController/SearchViewControllers.swift
+++ collectionPhoto/ViewControllers/TabBarViewController/SearchViewControllers.swift
@@ -5,4 +5,15 @@
 //  Created by Роман Назаров on 30.07.2022.
 //
 
-import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        title = "Поиск"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
