acf1b2170af652c752db2c1025086e8129772f28
Roman Nazarov <ste7ww@gmail.com>
2022-07-30 13:14:18 +0500

    headerView

--- collectionPhoto/ViewControllers/Album/HeaderView.swift
+++ collectionPhoto/ViewControllers/Album/HeaderView.swift
@@ -5,4 +5,34 @@
 //  Created by Роман Назаров on 30.07.2022.
 //
 
-import Foundation
+import UIKit
+
+class HeaderView: UICollectionReusableView {
+    static let reuseIdentifier = "header-reuse-identifier"
+    
+    let label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        //    label.adjustsFontForContentSizeCategory = true
+        label.font = UIFont.preferredFont(forTextStyle: .title2)
+        
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            label.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            label.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10)
+        ])
+    }
+}
+
